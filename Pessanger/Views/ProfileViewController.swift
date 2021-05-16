25f489846bc8ffcc1e083f67af0fc792568961c0
kms0524 <gcong2011@gmail.com>
2021-05-16 17:44:06 +0900

    뷰 이름 변경 및 Settings뷰 틀 생성

--- Pessanger/Views/ProfileViewController.swift
+++ Pessanger/Views/ProfileViewController.swift
@@ -1,27 +1,18 @@
 //
-//  ProfileViewController.swift
+//  ActualProfileViewController.swift
 //  Pessanger
 //
-//  Created by 강민성 on 2021/05/15.
+//  Created by 강민성 on 2021/05/16.
 //
 
 import UIKit
 
 class ProfileViewController: UIViewController {
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    setupNavigation()
-    // 임시
-    view.backgroundColor = .blue
-  }
-  
-  // MARK: Setup
-  fileprivate func setupNavigation() {
-    navigationController?.navigationBar.tintColor = .black
-  }
 
-    // 임시
-    view.backgroundColor = .blue
-  }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+
 }
