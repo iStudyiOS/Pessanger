e3243b2b0b318b66e9e9977802036dd633129d63
kms0524 <gcong2011@gmail.com>
2021-05-20 14:28:27 +0900

    KAKAO 지도 제거

--- Pessanger/Views/ProfileViewController.swift
+++ Pessanger/Views/ProfileViewController.swift
@@ -9,18 +9,5 @@ import UIKit
 import SnapKit
 
 class ProfileViewController: UIViewController {
-<<<<<<< HEAD
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-    }
-    
-
-=======
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-  }
->>>>>>> 3ac318330d3addcd6d25c67d5df7818a5118d3c0
 }
--- Pessanger/Views/SettingsViewController.swift
+++ Pessanger/Views/SettingsViewController.swift
@@ -8,9 +8,6 @@
 import UIKit
 
 class SettingsViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
-<<<<<<< HEAD
-    
-    
     
     private let tableView: UITableView = {
         let tableView = UITableView(frame: .zero, style: .grouped)
@@ -33,7 +30,6 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
     // MARK: Setup
     fileprivate func setupNavigation() {
         navigationController?.navigationBar.tintColor = .label
-
     }
     
     private func configureModels() {
@@ -43,14 +39,11 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
             }
         })]))
         
-        
         sections.append(Section(title: "계정", options: [Option(title: "로그아웃", handler: { [weak self] in
             DispatchQueue.main.async {
                 self?.signOutTapped()
             }
         })]))
-        
-        
     }
     
     private func viewProfile() {
@@ -59,7 +52,6 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
         vc.navigationItem.largeTitleDisplayMode = .never
         navigationController?.pushViewController(vc, animated: true)
     }
-    
     
     private func signOutTapped() {
         let alert = UIAlertController(title: "로그아웃", message: "로그아웃 하시겠습니까?", preferredStyle: .alert)
@@ -72,11 +64,9 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
     override func viewDidLayoutSubviews() {
         super.viewDidLayoutSubviews()
         tableView.frame = view.bounds
-        
     }
     
     // MARK: -TableView
-    
     func numberOfSections(in tableView: UITableView) -> Int {
         return sections.count
     }
@@ -103,92 +93,4 @@ class SettingsViewController: UIViewController, UITableViewDelegate, UITableView
         let model = sections[section]
         return model.title
     }
- 
-    
-=======
-  private let tableView: UITableView = {
-    let tableView = UITableView(frame: .zero, style: .grouped)
-    tableView.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
-    return tableView
-  }()
-  
-  private var sections = [Section]()
-  
-  override func viewDidLoad() {
-    super.viewDidLoad()
-    setupNavigation()
-    configureModels()
-    title = "설정"
-    view.addSubview(tableView)
-    tableView.delegate = self
-    tableView.dataSource = self
-  }
-  
-  // MARK: Setup
-  fileprivate func setupNavigation() {
-    navigationController?.navigationBar.tintColor = .label
-  }
-  
-  private func configureModels() {
-    sections.append(Section(title: "프로필", options: [Option(title: "프로필 보기", handler: { [weak self] in
-      DispatchQueue.main.async {
-        self?.viewProfile()
-      }
-    })]))
-    
-    sections.append(Section(title: "계정", options: [Option(title: "로그아웃", handler: { [weak self] in
-      DispatchQueue.main.async {
-        self?.signOutTapped()
-      }
-    })]))
-  }
-  
-  private func viewProfile() {
-    let vc = ProfileViewController()
-    vc.title = "프로필"
-    vc.navigationItem.largeTitleDisplayMode = .never
-    navigationController?.pushViewController(vc, animated: true)
-  }
-  
-  private func signOutTapped() {
-    let alert = UIAlertController(title: "로그아웃", message: "로그아웃 하시겠습니까?", preferredStyle: .alert)
-    alert.addAction(UIAlertAction(title: "취소", style: .cancel, handler: nil))
-    alert.addAction(UIAlertAction(title: "로그아웃", style: .destructive, handler: { _ in
-      
-    } ))
-  }
-  
-  override func viewDidLayoutSubviews() {
-    super.viewDidLayoutSubviews()
-    tableView.frame = view.bounds
-  }
-  
-  // MARK: -TableView
-  func numberOfSections(in tableView: UITableView) -> Int {
-    return sections.count
-  }
-  
-  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return sections[section].options.count
-  }
-  
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    let model = sections[indexPath.section].options[indexPath.row]
-    let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath)
-    cell.textLabel?.text = model.title
-    return cell
-  }
-  
-  func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-    tableView.deselectRow(at: indexPath, animated: true)
-    
-    let model = sections[indexPath.section].options[indexPath.row]
-    model.handler()
-  }
-  
-  func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-    let model = sections[section]
-    return model.title
-  }
->>>>>>> 3ac318330d3addcd6d25c67d5df7818a5118d3c0
 }
